4b96b59a797914d6c356c2b6d194ff65f22f8bba
Shabbir Abbas <shabbirabbas048@gmail.com>
2024-09-26 04:31:23 -0700

    implemented molecule search functionality

---
+++ frontend/src/components/MoleculeSearch/MoleculeSearch.js
@@ -0,0 +1,94 @@
+import React, { useState, useEffect, useRef} from 'react';
+import './MoleculeSearch.css';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+const MoleculeSearch = ({ onMoleculeSelect, selectedMolecules }) => {
+  const [searchText, setSearchText] = useState('');
+  const [molecules, setMolecules] = useState([]);
+  const [showMoleculeList, setShowMoleculeList] = useState(false);
+  const searchContainerRef = useRef(null);
+
+  const searchMolecules = async (query) => {
+    try {
+      const response = await fetch(
+        `/api/v1/patients/molecules/search?query=${query}`
+      );
+      if (!response.ok) {
+        throw new Error('Failed to fetch molecules');
+      }
+      const data = await response.json();
+      setMolecules(data);
+    } catch (error) {
+      console.error('Error fetching molecules:', error);
+      toast.error('Failed to fetch molecules. Please try again later.');
+    }
+  };
+
+  const handleSelect = (moleculeId) => {
+    onMoleculeSelect(moleculeId);
+  };
+
+  const isSelected = (moleculeId) => {
+    return selectedMolecules.some((allergy) =>
+      typeof allergy === 'object'
+        ? allergy.id === moleculeId
+        : allergy === moleculeId
+    );
+  };
+
+  const handleClickOutside = (event) => {
+    if (
+      searchContainerRef.current &&
+      !searchContainerRef.current.contains(event.target)
+    ) {
+        setShowMoleculeList(false); 
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener('mousedown', handleClickOutside);
+
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, []);
+
+  const handleFocus = () => {
+    setShowMoleculeList(true);
+    if (searchText == '') {
+      searchMolecules('');
+    }
+  };
+
+  return (
+    <div className='molecule-search' ref={searchContainerRef}>
+      <input
+        type='text'
+        placeholder='Search for a molecule...'
+        value={searchText}
+        onFocus={handleFocus}
+        onChange={(e) => {
+          setSearchText(e.target.value);
+          searchMolecules(e.target.value);
+        }}
+      />
+      {showMoleculeList && (
+        <ul className='molecule-list'>
+          {molecules.map((molecule) => (
+            <li
+              key={molecule.id}
+              onClick={() => handleSelect(molecule.id)}
+              className={isSelected(molecule.id) ? 'selected' : ''}
+            >
+              {molecule.name}
+            </li>
+          ))}
+        </ul>
+      )}
+      <ToastContainer />
+    </div>
+  );
+};
+
+export default MoleculeSearch;
--- frontend/src/components/PatientProfile/PatientProfile.js
+++ frontend/src/components/PatientProfile/PatientProfile.js
@@ -2,33 +2,104 @@ import React, {useEffect, useState} from 'react';
 import {useParams} from 'react-router-dom';
 import ProfileItem from '../ProfileItem/ProfileItem';
 import './PatientProfile.css';
+import MoleculeSearch from '../MoleculeSearch/MoleculeSearch';
+import './PatientProfile.css';
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const PatientProfile = () => {
-  const {id} = useParams();
+  const { id } = useParams();
   const [patientData, setPatientData] = useState(null);
   const [errorMessage, setErrorMessage] = useState(null);
+  const [allergies, setAllergies] = useState([]); 
+
+  const fetchData = () => {
+    fetch(`/api/v1/patients/${id}`)
+      .then((patientResponse) => {
+        if (!patientResponse.ok) {
+          throw new Error('Failed to fetch patient data');
+        }
+        return patientResponse.json();
+      })
+      .then((patientData) => {
+        setPatientData(patientData);
+
+        return fetch(`/api/v1/patients/${id}/allergies`);
+      })
+      .then((allergiesResponse) => {
+        if (!allergiesResponse.ok) {
+          throw new Error('Failed to fetch allergies');
+        }
+        return allergiesResponse.json();
+      })
+      .then((allergiesData) => {
+        setAllergies(allergiesData);
+      })
+      .catch((error) => {
+        setErrorMessage({
+          title: 'Error fetching data',
+          details: error.message,
+        });
+      });
+  };
 
   useEffect(() => {
-    fetch(`/api/v1/patients/${id}`, {
-      method: "GET",
-      mode: "cors",
-    })
-      .then(response => response.json())
-      .then(data => setPatientData(data))
-      .catch(error => {
-        console.error(error);
-        setErrorMessage({title: "Error fetching patient data", details: error.message});
-      });
+    fetchData();
   }, [id]);
 
+  const addMoleculeToAllergies = async (moleculeId) => {
+    if (!patientData.allergies) {
+      patientData.allergies = [];
+    }
+
+    const existingAllergies = (patientData.allergies || []).map((allergy) =>
+      typeof allergy === 'object' ? allergy.id : allergy
+    );
+
+    const combinedAllergies = [
+      ...allergies.map((allergy) =>
+        typeof allergy === 'object' ? allergy.id : allergy
+      ),
+      ...existingAllergies,
+    ];
+
+    if (combinedAllergies.includes(moleculeId)) {
+      toast.info('This molecule is already added to the allergies list.');
+      return;
+    }
+
+    const updatedAllergies = [...new Set([...combinedAllergies, moleculeId])];
+
+    try {
+      const response = await fetch(`/api/v1/patients/${id}/allergies`, {
+        method: 'PUT',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(updatedAllergies),
+      });
+
+      if (!response.ok) {
+        throw new Error('Failed to update allergies');
+      }
+
+      const data = await response.json();
+      setPatientData((prevState) => ({ ...prevState, allergies: data }));
+      toast.success('Molecule successfully added to allergies');
+    } catch (error) {
+      console.error('Error updating allergies:', error);
+      toast.error('Failed to update allergies. Please try again later.');
+    }
+  };
+
   if (errorMessage) {
-    return <div className="container">
-      <h1>{errorMessage.title}</h1>
-      <p>{errorMessage.details}</p>
-    </div>
+    return (
+      <div className='container'>
+        <h1>{errorMessage.title}</h1>
+        <p>{errorMessage.details}</p>
+      </div>
+    );
   }
   if (!patientData) {
-    return <div className="container">Loading...</div>;
+    return <div className='container'>Loading...</div>;
   }
 
   return (
@@ -41,6 +112,12 @@ const PatientProfile = () => {
         <ProfileItem label="Birth Date" value={patientData.birth_date}/>
         <ProfileItem label="Height" value={patientData.height}/>
         <ProfileItem label="Weight" value={patientData.weight}/>
+
+        <MoleculeSearch
+          onMoleculeSelect={addMoleculeToAllergies}
+          selectedMolecules={[...allergies, ...(patientData.allergies || [])]}
+        />
+
       </div>
     </div>
   );
